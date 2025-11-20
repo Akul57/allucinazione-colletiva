2ab86a1f9e87e985e72282552066d566cb666530
chitokity@gmail.com <chitokity@gmail.com>
2025-11-20 22:11:27 +0100

    patch words

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,146 +1,176 @@
-import { GameData } from '../types';
+import { GameData } from "../types";
 
 // DATABASE DI PAROLE STATICHE
 // Regola: Stessa categoria, ma forma/funzione diversa. NO sinonimi stretti.
 const WORD_PAIRS: GameData[] = [
-  // Oggetti Casa
-  {
-    category: 'Illuminazione',
-    normalWord: 'Lampadina',
-    similarWord: 'Candela',
-  },
-  { category: 'Sedute', normalWord: 'Sedia', similarWord: 'Sgabello' },
-  { category: 'Posate', normalWord: 'Forchetta', similarWord: 'Cucchiaio' },
-  { category: 'Pulizia', normalWord: 'Scopa', similarWord: 'Aspirapolvere' },
-  {
-    category: 'Contenitori Liquidi',
-    normalWord: 'Bicchiere',
-    similarWord: 'Tazza',
-  },
-  { category: 'Dormire', normalWord: 'Cuscino', similarWord: 'Coperta' },
-  { category: 'Bagno', normalWord: 'Sapone', similarWord: 'Shampoo' },
-  { category: 'Scrittura', normalWord: 'Penna', similarWord: 'Matita' },
-  {
-    category: 'Tempo',
-    normalWord: 'Orologio da Polso',
-    similarWord: 'Sveglia',
-  },
-  {
-    category: 'Accessori Pioggia',
-    normalWord: 'Ombrello',
-    similarWord: 'Impermeabile',
-  },
+  // --- CIBO E CUCINA ---
+  { category: "Colazione", normalWord: "Pancake", similarWord: "Waffle" },
+  { category: "Colazione Dolce", normalWord: "Cornetto", similarWord: "Ciambella" },
+  { category: "Frutta Tonda", normalWord: "Arancia", similarWord: "Pesca" },
+  { category: "Frutta Piccola", normalWord: "Fragola", similarWord: "Lampone" },
+  { category: "Frutta Esotica", normalWord: "Ananas", similarWord: "Cocco" },
+  { category: "Verdura Verde", normalWord: "Broccolo", similarWord: "Cavolfiore" },
+  { category: "Verdura Lunga", normalWord: "Cetriolo", similarWord: "Zucchina" },
+  { category: "Sapori Forti", normalWord: "Aglio", similarWord: "Cipolla" },
+  { category: "Bevande Calde", normalWord: "Caffè", similarWord: "Tè" },
+  { category: "Fast Food", normalWord: "Hamburger", similarWord: "Hot Dog" },
+  { category: "Cucina Giapponese", normalWord: "Sushi", similarWord: "Ramen" },
+  { category: "Dolci Freddi", normalWord: "Gelato", similarWord: "Ghiacciolo" },
+  { category: "Carboidrati", normalWord: "Pasta", similarWord: "Riso" },
+  { category: "Pasta Lunga", normalWord: "Spaghetti", similarWord: "Tagliatelle" },
+  { category: "Condimenti", normalWord: "Ketchup", similarWord: "Maionese" },
+  { category: "Frutta Estiva", normalWord: "Anguria", similarWord: "Melone" },
+  { category: "Latticini", normalWord: "Latte", similarWord: "Yogurt" },
+  { category: "Formaggi", normalWord: "Mozzarella", similarWord: "Parmigiano" },
+  { category: "Snack Salati", normalWord: "Patatine", similarWord: "Popcorn" },
+  { category: "Dolci da Forno", normalWord: "Torta", similarWord: "Biscotto" },
+  { category: "Carne", normalWord: "Bistecca", similarWord: "Salsiccia" },
+  { category: "Pesce Cotto", normalWord: "Fritto Misto", similarWord: "Grigliata" },
+  { category: "Pentolame", normalWord: "Padella", similarWord: "Pentola" },
 
-  // Cibo e Bevande
-  { category: 'Colazione', normalWord: 'Pancake', similarWord: 'Waffle' },
-  { category: 'Frutta Tonda', normalWord: 'Arancia', similarWord: 'Pesca' },
-  { category: 'Bevande Calde', normalWord: 'Caffè', similarWord: 'Tè' },
-  { category: 'Fast Food', normalWord: 'Hamburger', similarWord: 'Hot Dog' },
-  { category: 'Dolci Freddi', normalWord: 'Gelato', similarWord: 'Ghiacciolo' },
-  { category: 'Carboidrati', normalWord: 'Pasta', similarWord: 'Riso' },
-  { category: 'Condimenti', normalWord: 'Ketchup', similarWord: 'Maionese' },
-  { category: 'Verdura', normalWord: 'Cetriolo', similarWord: 'Zucchina' },
-  { category: 'Frutta Estiva', normalWord: 'Anguria', similarWord: 'Melone' },
-  { category: 'Latticini', normalWord: 'Latte', similarWord: 'Yogurt' },
+  // --- OGGETTI DI CASA ---
+  { category: "Illuminazione", normalWord: "Lampadina", similarWord: "Candela" },
+  { category: "Sedute", normalWord: "Sedia", similarWord: "Sgabello" },
+  { category: "Relax", normalWord: "Divano", similarWord: "Poltrona" },
+  { category: "Posate", normalWord: "Forchetta", similarWord: "Cucchiaio" },
+  { category: "Pulizia", normalWord: "Scopa", similarWord: "Aspirapolvere" },
+  { category: "Pulizia Tessuti", normalWord: "Spugna", similarWord: "Straccio" },
+  { category: "Contenitori Liquidi", normalWord: "Bicchiere", similarWord: "Tazza" },
+  { category: "Contenitori Chiusi", normalWord: "Bottiglia", similarWord: "Borraccia" },
+  { category: "Dormire", normalWord: "Cuscino", similarWord: "Coperta" },
+  { category: "Bagno", normalWord: "Sapone", similarWord: "Shampoo" },
+  { category: "Igiene Orale", normalWord: "Spazzolino", similarWord: "Dentifricio" },
+  { category: "Asciugarsi", normalWord: "Asciugamano", similarWord: "Accappatoio" },
+  { category: "Tempo", normalWord: "Orologio da Polso", similarWord: "Sveglia" },
+  { category: "Accessori Pioggia", normalWord: "Ombrello", similarWord: "Impermeabile" },
+  { category: "Sicurezza", normalWord: "Chiave", similarWord: "Lucchetto" },
+  { category: "Arredamento", normalWord: "Tappeto", similarWord: "Tenda" },
+  { category: "Elettrodomestici", normalWord: "Forno", similarWord: "Microonde" },
+  { category: "Bucato", normalWord: "Lavatrice", similarWord: "Stendino" },
 
-  // Trasporti
-  { category: 'Aria', normalWord: 'Aereo', similarWord: 'Elicottero' },
-  { category: 'Rotaie', normalWord: 'Treno', similarWord: 'Tram' },
-  { category: 'Acqua', normalWord: 'Nave', similarWord: 'Sottomarino' },
-  {
-    category: 'Due Ruote',
-    normalWord: 'Bicicletta',
-    similarWord: 'Monopattino',
-  },
-  {
-    category: 'Emergenza',
-    normalWord: 'Ambulanza',
-    similarWord: 'Auto della Polizia',
-  },
-  { category: 'Pubblici', normalWord: 'Autobus', similarWord: 'Taxi' },
-  { category: 'Neve', normalWord: 'Sci', similarWord: 'Snowboard' },
-  { category: 'Spazio', normalWord: 'Razzo', similarWord: 'Satellite' },
+  // --- SCUOLA E UFFICIO ---
+  { category: "Scrittura", normalWord: "Penna", similarWord: "Matita" },
+  { category: "Supporti", normalWord: "Quaderno", similarWord: "Foglio" },
+  { category: "Taglio", normalWord: "Forbici", similarWord: "Taglierino" },
+  { category: "Fissaggio Carta", normalWord: "Spillatrice", similarWord: "Scotch" },
+  { category: "Contenitori", normalWord: "Zaino", similarWord: "Valigia" },
+  { category: "Geometria", normalWord: "Righello", similarWord: "Compasso" },
+  { category: "Cancellare", normalWord: "Gomma", similarWord: "Bianchetto" },
 
-  // Animali (Forme diverse)
-  { category: 'Volatili', normalWord: 'Aquila', similarWord: 'Pipistrello' },
-  { category: 'Insetti Volanti', normalWord: 'Ape', similarWord: 'Mosca' },
-  { category: 'Acquatici', normalWord: 'Delfino', similarWord: 'Squalo' },
-  { category: 'Lenti', normalWord: 'Tartaruga', similarWord: 'Lumaca' },
-  { category: 'Domestici', normalWord: 'Cane', similarWord: 'Gatto' },
-  { category: 'Fattoria', normalWord: 'Mucca', similarWord: 'Cavallo' },
-  { category: 'Striscianti', normalWord: 'Serpente', similarWord: 'Verme' },
-  { category: 'Artici', normalWord: 'Pinguino', similarWord: 'Foca' },
+  // --- TRASPORTI ---
+  { category: "Aria", normalWord: "Aereo", similarWord: "Elicottero" },
+  { category: "Rotaie", normalWord: "Treno", similarWord: "Tram" },
+  { category: "Acqua Grande", normalWord: "Nave", similarWord: "Sottomarino" },
+  { category: "Acqua Piccola", normalWord: "Barca a Vela", similarWord: "Gommone" },
+  { category: "Acqua Venezia", normalWord: "Canoa", similarWord: "Gondola" },
+  { category: "Due Ruote", normalWord: "Bicicletta", similarWord: "Monopattino" },
+  { category: "Motori a 2 Ruote", normalWord: "Moto", similarWord: "Scooter" },
+  { category: "Emergenza", normalWord: "Ambulanza", similarWord: "Auto della Polizia" },
+  { category: "Pubblici", normalWord: "Autobus", similarWord: "Taxi" },
+  { category: "Neve", normalWord: "Sci", similarWord: "Snowboard" },
+  { category: "Spazio", normalWord: "Razzo", similarWord: "Satellite" },
+  { category: "Agricoltura", normalWord: "Trattore", similarWord: "Camion" },
 
-  // Tecnologia & Elettronica
-  { category: 'Computer', normalWord: 'Tastiera', similarWord: 'Mouse' },
-  { category: 'Dispositivi', normalWord: 'Smartphone', similarWord: 'Tablet' },
-  { category: 'Audio', normalWord: 'Cuffie', similarWord: 'Altoparlante' },
-  {
-    category: 'Immagini',
-    normalWord: 'Fotocamera',
-    similarWord: 'Videocamera',
-  },
-  {
-    category: 'Archiviazione',
-    normalWord: 'Chiavetta USB',
-    similarWord: 'Hard Disk',
-  },
+  // --- ANIMALI ---
+  { category: "Volatili", normalWord: "Aquila", similarWord: "Pipistrello" },
+  { category: "Uccelli Notturni", normalWord: "Gufo", similarWord: "Civetta" },
+  { category: "Insetti Volanti", normalWord: "Ape", similarWord: "Mosca" },
+  { category: "Insetti Fastidiosi", normalWord: "Zanzara", similarWord: "Vespa" },
+  { category: "Insetti Terra", normalWord: "Ragno", similarWord: "Scorpione" },
+  { category: "Acquatici", normalWord: "Delfino", similarWord: "Squalo" },
+  { category: "Crostacei", normalWord: "Granchio", similarWord: "Aragosta" },
+  { category: "Lenti", normalWord: "Tartaruga", similarWord: "Lumaca" },
+  { category: "Domestici", normalWord: "Cane", similarWord: "Gatto" },
+  { category: "Fattoria", normalWord: "Mucca", similarWord: "Cavallo" },
+  { category: "Fattoria Piccoli", normalWord: "Gallina", similarWord: "Anatra" },
+  { category: "Striscianti", normalWord: "Serpente", similarWord: "Verme" },
+  { category: "Artici", normalWord: "Pinguino", similarWord: "Foca" },
+  { category: "Savana", normalWord: "Leone", similarWord: "Tigre" },
+  { category: "Grandi Mammiferi", normalWord: "Elefante", similarWord: "Ippopotamo" },
+  { category: "Bosco", normalWord: "Lupo", similarWord: "Volpe" },
+  { category: "Salterini", normalWord: "Canguro", similarWord: "Coniglio" },
 
-  // Abbigliamento
-  { category: 'Mani', normalWord: 'Guanti', similarWord: 'Muffole' },
-  { category: 'Testa', normalWord: 'Cappello', similarWord: 'Casco' },
-  { category: 'Piedi Estivi', normalWord: 'Sandali', similarWord: 'Infradito' },
-  {
-    category: 'Piedi Invernali',
-    normalWord: 'Stivali',
-    similarWord: 'Scarponi',
-  },
-  { category: 'Gambe', normalWord: 'Pantaloni', similarWord: 'Pantaloncini' },
-  { category: 'Sopra', normalWord: 'Felpa', similarWord: 'Maglione' },
-  { category: 'Collo', normalWord: 'Sciarpa', similarWord: 'Cravatta' },
+  // --- TECNOLOGIA ---
+  { category: "Computer", normalWord: "Tastiera", similarWord: "Mouse" },
+  { category: "Dispositivi", normalWord: "Smartphone", similarWord: "Tablet" },
+  { category: "Audio", normalWord: "Cuffie", similarWord: "Altoparlante" },
+  { category: "Immagini", normalWord: "Fotocamera", similarWord: "Videocamera" },
+  { category: "Archiviazione", normalWord: "Chiavetta USB", similarWord: "Hard Disk" },
+  { category: "Retro Tech", normalWord: "Vinile", similarWord: "Cassetta" },
+  { category: "Schermi", normalWord: "Televisione", similarWord: "Proiettore" },
 
-  // Sport e Hobby
-  { category: 'Racchette', normalWord: 'Tennis', similarWord: 'Ping Pong' },
-  { category: 'Palla a squadre', normalWord: 'Calcio', similarWord: 'Basket' },
-  { category: 'Acqua', normalWord: 'Nuoto', similarWord: 'Pallanuoto' },
-  { category: 'Combattimento', normalWord: 'Pugilato', similarWord: 'Karate' },
-  { category: 'Musica Corde', normalWord: 'Chitarra', similarWord: 'Violino' },
-  {
-    category: 'Musica Tasti',
-    normalWord: 'Pianoforte',
-    similarWord: 'Fisarmonica',
-  },
-  { category: 'Musica Fiato', normalWord: 'Tromba', similarWord: 'Sassofono' },
-  { category: 'Gioco da Tavolo', normalWord: 'Scacchi', similarWord: 'Dama' },
+  // --- ABBIGLIAMENTO ---
+  { category: "Mani", normalWord: "Guanti", similarWord: "Muffole" },
+  { category: "Testa", normalWord: "Cappello", similarWord: "Casco" },
+  { category: "Testa Inverno", normalWord: "Berretto", similarWord: "Paraorecchie" },
+  { category: "Piedi Estivi", normalWord: "Sandali", similarWord: "Infradito" },
+  { category: "Piedi Invernali", normalWord: "Stivali", similarWord: "Scarponi" },
+  { category: "Gambe", normalWord: "Pantaloni", similarWord: "Pantaloncini" },
+  { category: "Sopra", normalWord: "Felpa", similarWord: "Maglione" },
+  { category: "Collo", normalWord: "Sciarpa", similarWord: "Cravatta" },
+  { category: "Accessori", normalWord: "Cintura", similarWord: "Bretelle" },
+  { category: "Gioielli", normalWord: "Collana", similarWord: "Braccialetto" },
+  { category: "Vista", normalWord: "Occhiali", similarWord: "Lenti a Contatto" },
+  { category: "Intimo", normalWord: "Calzini", similarWord: "Mutande" },
 
-  // Luoghi
-  { category: 'Spettacolo', normalWord: 'Cinema', similarWord: 'Teatro' },
-  { category: 'Studio', normalWord: 'Biblioteca', similarWord: 'Libreria' },
-  { category: 'Acqua', normalWord: 'Mare', similarWord: 'Piscina' },
-  { category: 'Cibo Fuori', normalWord: 'Ristorante', similarWord: 'Mensa' },
-  { category: 'Verde', normalWord: 'Parco', similarWord: 'Foresta' },
-  { category: 'Altezze', normalWord: 'Montagna', similarWord: 'Collina' },
-  { category: 'Abitazione', normalWord: 'Castello', similarWord: 'Palazzo' },
+  // --- SPORT E HOBBY ---
+  { category: "Racchette", normalWord: "Tennis", similarWord: "Ping Pong" },
+  { category: "Palla a squadre", normalWord: "Calcio", similarWord: "Basket" },
+  { category: "Palle Pesanti", normalWord: "Bowling", similarWord: "Bocce" },
+  { category: "Acqua", normalWord: "Nuoto", similarWord: "Pallanuoto" },
+  { category: "Tavole", normalWord: "Surf", similarWord: "Skate" },
+  { category: "Combattimento", normalWord: "Pugilato", similarWord: "Karate" },
+  { category: "Musica Corde", normalWord: "Chitarra", similarWord: "Violino" },
+  { category: "Musica Tasti", normalWord: "Pianoforte", similarWord: "Fisarmonica" },
+  { category: "Musica Fiato", normalWord: "Tromba", similarWord: "Sassofono" },
+  { category: "Percussioni", normalWord: "Batteria", similarWord: "Tamburo" },
+  { category: "Gioco da Tavolo", normalWord: "Scacchi", similarWord: "Dama" },
+  { category: "Gioco Carte", normalWord: "Poker", similarWord: "Uno" },
+  { category: "Arte", normalWord: "Pittura", similarWord: "Scultura" },
+  
+  // --- LUOGHI ---
+  { category: "Spettacolo", normalWord: "Cinema", similarWord: "Teatro" },
+  { category: "Studio", normalWord: "Biblioteca", similarWord: "Libreria" },
+  { category: "Acqua", normalWord: "Mare", similarWord: "Piscina" },
+  { category: "Acqua Dolce", normalWord: "Lago", similarWord: "Fiume" },
+  { category: "Cibo Fuori", normalWord: "Ristorante", similarWord: "Mensa" },
+  { category: "Verde", normalWord: "Parco", similarWord: "Foresta" },
+  { category: "Altezze", normalWord: "Montagna", similarWord: "Collina" },
+  { category: "Abitazione", normalWord: "Castello", similarWord: "Palazzo" },
+  { category: "Abitazione Mobile", normalWord: "Camper", similarWord: "Tenda" },
+  { category: "Sacro", normalWord: "Chiesa", similarWord: "Tempio" },
+  { category: "Commercio", normalWord: "Supermercato", similarWord: "Mercato" },
+  { category: "Salute", normalWord: "Ospedale", similarWord: "Farmacia" },
 
-  // Strumenti da Lavoro
-  { category: 'Taglio', normalWord: 'Forbici', similarWord: 'Coltello' },
-  { category: 'Fissaggio', normalWord: 'Martello', similarWord: 'Cacciavite' },
-  { category: 'Giardinaggio', normalWord: 'Pala', similarWord: 'Rastrello' },
-  {
-    category: 'Medicale',
-    normalWord: 'Stetoscopio',
-    similarWord: 'Termometro',
-  },
+  // --- STRUMENTI E LAVORO ---
+  { category: "Fissaggio", normalWord: "Martello", similarWord: "Cacciavite" },
+  { category: "Giardinaggio", normalWord: "Pala", similarWord: "Rastrello" },
+  { category: "Medicale", normalWord: "Stetoscopio", similarWord: "Termometro" },
+  { category: "Pittura", normalWord: "Pennello", similarWord: "Rullo" },
+  { category: "Professioni", normalWord: "Dottore", similarWord: "Infermiere" },
+  { category: "Divisa", normalWord: "Poliziotto", similarWord: "Vigile del Fuoco" },
+  
+  // --- FANTASY E HALLOWEEN ---
+  { category: "Mostri", normalWord: "Vampiro", similarWord: "Lupo Mannaro" },
+  { category: "Non Morti", normalWord: "Fantasma", similarWord: "Zombie" },
+  { category: "Magia", normalWord: "Bacchetta Magica", similarWord: "Pozione" },
+  { category: "Fiabe", normalWord: "Drago", similarWord: "Unicorno" },
+  { category: "Reali", normalWord: "Re", similarWord: "Regina" },
 
-  // Concetti Astratti / Vari
-  { category: 'Eventi', normalWord: 'Matrimonio', similarWord: 'Funerale' },
-  { category: 'Astri', normalWord: 'Sole', similarWord: 'Luna' },
-  { category: 'Elementi', normalWord: 'Fuoco', similarWord: 'Fulmine' },
-  { category: 'Soldi', normalWord: 'Banconota', similarWord: 'Moneta' },
+  // --- CONCETTI E NATURA ---
+  { category: "Eventi", normalWord: "Matrimonio", similarWord: "Funerale" },
+  { category: "Astri", normalWord: "Sole", similarWord: "Luna" },
+  { category: "Cielo", normalWord: "Nuvola", similarWord: "Nebbia" },
+  { category: "Meteo", normalWord: "Pioggia", similarWord: "Neve" },
+  { category: "Elementi", normalWord: "Fuoco", similarWord: "Fulmine" },
+  { category: "Soldi", normalWord: "Banconota", similarWord: "Moneta" },
+  { category: "Anatomia", normalWord: "Cuore", similarWord: "Cervello" },
+  { category: "Viso", normalWord: "Occhio", similarWord: "Bocca" },
+  { category: "Arti", normalWord: "Braccio", similarWord: "Gamba" }
 ];
 
 // Funzione helper per mescolare l'array (Fisher-Yates shuffle)
-// Utile se volessimo evitare ripetizioni nella stessa sessione,
+// Utile se volessimo evitare ripetizioni nella stessa sessione, 
 // ma per ora prendiamo solo un elemento random.
 const getRandomElement = <T>(arr: T[]): T => {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -152,16 +182,16 @@ export const generateGameScenario = async (): Promise<GameData> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       const scenario = getRandomElement(WORD_PAIRS);
-
+      
       // Decidiamo casualmente chi prende la parola A e chi la B per variare ancora di più
       // Esempio: A volte i buoni hanno "Chitarra", a volte "Violino".
       const flip = Math.random() > 0.5;
-
+      
       if (flip) {
         resolve({
           category: scenario.category,
           normalWord: scenario.similarWord,
-          similarWord: scenario.normalWord,
+          similarWord: scenario.normalWord
         });
       } else {
         resolve(scenario);
